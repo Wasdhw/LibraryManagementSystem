52a3c58cb494db9902117bc6b250b637fe7b1a12
unknown <francismanibo09@gmail.com>
2025-10-06 11:58:46 +0800

    #1

--- LibraryManagementSystem/Dashboard.Designer.cs
+++ LibraryManagementSystem/Dashboard.Designer.cs
@@ -32,25 +32,25 @@ namespace LibraryManagementSystem
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
             this.dashboard_RB = new System.Windows.Forms.Label();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.label6 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
             this.dashboard_IB = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label4 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.dashboard_AB = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             this.panel4.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -88,15 +88,6 @@ namespace LibraryManagementSystem
             this.dashboard_RB.TabIndex = 2;
             this.dashboard_RB.Text = "0";
             // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = global::LibraryManagementSystem.Properties.Resources.icons8_book_60px_2;
-            this.pictureBox3.Location = new System.Drawing.Point(14, 37);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(60, 60);
-            this.pictureBox3.TabIndex = 1;
-            this.pictureBox3.TabStop = false;
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
@@ -129,15 +120,6 @@ namespace LibraryManagementSystem
             this.dashboard_IB.Size = new System.Drawing.Size(30, 33);
             this.dashboard_IB.TabIndex = 2;
             this.dashboard_IB.Text = "0";
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::LibraryManagementSystem.Properties.Resources.icons8_books_60px_3;
-            this.pictureBox2.Location = new System.Drawing.Point(14, 37);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(60, 60);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
             // 
             // label4
             // 
@@ -172,15 +154,6 @@ namespace LibraryManagementSystem
             this.dashboard_AB.TabIndex = 2;
             this.dashboard_AB.Text = "0";
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::LibraryManagementSystem.Properties.Resources.icons8_book_stack_60px;
-            this.pictureBox1.Location = new System.Drawing.Point(14, 37);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(60, 60);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -194,13 +167,40 @@ namespace LibraryManagementSystem
             // 
             // pictureBox4
             // 
-            this.pictureBox4.Image = global::LibraryManagementSystem.Properties.Resources.pexels_polina_zimmerman_3747468;
+            this.pictureBox4.Image = global::LibraryManagementSystem.Properties.Resources._551319363_2220571021741076_4897105495816530304_n;
             this.pictureBox4.Location = new System.Drawing.Point(13, 232);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(855, 313);
+            this.pictureBox4.Size = new System.Drawing.Size(851, 315);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 1;
             this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::LibraryManagementSystem.Properties.Resources.icons8_book_60px_2;
+            this.pictureBox3.Location = new System.Drawing.Point(14, 37);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::LibraryManagementSystem.Properties.Resources.icons8_books_60px_3;
+            this.pictureBox2.Location = new System.Drawing.Point(14, 37);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LibraryManagementSystem.Properties.Resources.icons8_book_stack_60px;
+            this.pictureBox1.Location = new System.Drawing.Point(14, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // Dashboard
             // 
@@ -213,14 +213,14 @@ namespace LibraryManagementSystem
             this.panel1.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
