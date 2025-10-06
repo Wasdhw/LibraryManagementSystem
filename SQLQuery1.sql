d6c43a0327c798573a3305c1991972fa0310b1db
Luna <Luna@DESKTOP-3UF6JFS>
2025-10-06 09:44:18 +0800

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE users (
+id INT PRIMARY KEY identity(1,1),
+email VARCHAR(max) NULL,
+username VARCHAR(max) NULL,
+password VARCHAR(max) NULL,
+date_register DATE NULL,
+
+
+);
+
+select * from users
